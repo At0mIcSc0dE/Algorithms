312421b5965d8ff273f0a7f4fe8d2d3a5497bdce
Simon <simon_geier@web.de>
2020-06-13 19:02:03 +0200

    First 'working' version of QuickSort algorithm

--- Algorithms/src/SortingAlgorithms/Quicksort.h
+++ Algorithms/src/SortingAlgorithms/Quicksort.h
@@ -1,33 +1,34 @@
 #include "pch.h"
 
-
-#define PIVOTINDEX unsortedArr.size() - 1
-
 std::vector<int> QuickSort(std::vector<int>& unsortedArr)
 {
 	TIMER;
-	unsigned int itemFromLeftIndex = 0;
-	unsigned int itemFromRightIndex = 0;
 
 	for (int i = 0; i < unsortedArr.size(); ++i)
 	{
+		unsigned int itemFromLeftIndex = 0;
+		unsigned int itemFromRightIndex = unsortedArr.size() - 1;
+
 		//Select pivot and move swap unsortedArr[unsortedArr.size() - 1] with pivot
 		int firstElement = unsortedArr[0];
 		int middleElement = unsortedArr[unsortedArr.size() / 2];
 		int lastElement = unsortedArr[unsortedArr.size() - 1];
 
-		if ((firstElement > lastElement && firstElement < middleElement) || (firstElement < lastElement && firstElement > middleElement))
+		
+		//If first element is pivot, move it to end
+		if ((firstElement > middleElement && firstElement < lastElement) || (firstElement < middleElement && firstElement > lastElement))
 		{
-			unsortedArr[unsortedArr.size() / 2] = firstElement;
-			unsortedArr[0] = middleElement;
+			unsortedArr[unsortedArr.size() - 1] = firstElement;
+			unsortedArr[0] = lastElement;
 		}
-		else if ((middleElement > lastElement && middleElement < firstElement) || (middleElement < lastElement && middleElement > firstElement))
+		//If the middle element if pivot, move it to end
+		else if ((middleElement > firstElement && middleElement < lastElement) || (middleElement < firstElement && middleElement > lastElement))
 		{
-			unsortedArr[unsortedArr.size() / 2] = lastElement;
 			unsortedArr[unsortedArr.size() - 1] = middleElement;
+			unsortedArr[unsortedArr.size() / 2] = lastElement;
 		}
 
-		if (itemFromLeftIndex < itemFromRightIndex)
+		while (itemFromLeftIndex < itemFromRightIndex)
 		{
 			//Item from left that is larger than the pivot
 			for (int j = 0; j < unsortedArr.size(); ++j)
@@ -40,7 +41,7 @@ std::vector<int> QuickSort(std::vector<int>& unsortedArr)
 			}
 
 			//Item from right that is smaller than the pivot
-			for(int j = unsortedArr.size() - 1; j > 0; --j)
+			for(int j = unsortedArr.size() - 2; j > -1; --j)
 			{
 				if (unsortedArr[j] < unsortedArr[unsortedArr.size() - 1])
 				{
@@ -48,17 +49,38 @@ std::vector<int> QuickSort(std::vector<int>& unsortedArr)
 					break;
 				}
 			}
+
+			if (itemFromLeftIndex > itemFromRightIndex)
+			{
+				break;
+			}
+
+			//Swap left and right value
+			int itemFromLeft = unsortedArr[itemFromLeftIndex];
+			unsortedArr[itemFromLeftIndex] = unsortedArr[itemFromRightIndex];
+			unsortedArr[itemFromRightIndex] = itemFromLeft;
+
 		}
 		//Swap pivot and item from left
-		else
+		int pivot = unsortedArr[unsortedArr.size() - 1];
+		unsortedArr[unsortedArr.size() - 1] = unsortedArr[itemFromLeftIndex];
+		unsortedArr[itemFromLeftIndex] = pivot;
+
+		//Check if completed
+		for (int j = 0; j <= itemFromLeftIndex; ++j)
 		{
-			int pivot = unsortedArr[unsortedArr.size() - 1];
-			unsortedArr[unsortedArr.size() - 1] = unsortedArr[itemFromLeftIndex];
-			unsortedArr[itemFromLeftIndex] = pivot;
+			if (unsortedArr[j] > unsortedArr[itemFromLeftIndex])
+				continue;
 		}
 
+		for (int j = itemFromLeftIndex + 1; j < unsortedArr.size(); ++j)
+		{
+			if (unsortedArr[j] < unsortedArr[itemFromLeftIndex])
+				continue;
+		}
+		break;
 
 	}
 
-	return { unsortedArr[itemFromLeftIndex], (int)itemFromLeftIndex };
+	return unsortedArr;
 }
--- Algorithms/src/main.cpp
+++ Algorithms/src/main.cpp
@@ -6,28 +6,55 @@
 
 #define LOG_ARRAY 1
 
-
 std::ostream& operator<<(std::ostream& os, std::vector<int>& vec)
 {
-	#if LOG_ARRAY
-		for (int& elem : vec)
-		{
-			std::cout << elem << ", ";
-		}
-	#endif
+#if LOG_ARRAY
+	for (int& elem : vec)
+	{
+		std::cout << elem << ", ";
+	}
+#endif
 	return os;
+}
+
+
+int GetPivot(std::vector<int>& unsortedArr)
+{
+	//Select pivot and move swap unsortedArr[unsortedArr.size() - 1] with pivot
+	int firstElement = unsortedArr[0];
+	int middleElement = unsortedArr[unsortedArr.size() / 2];
+	int lastElement = unsortedArr[unsortedArr.size() - 1];
+
+
+	//If first element is pivot, move it to end
+	if ((firstElement > middleElement && firstElement < lastElement) || (firstElement < middleElement && firstElement > lastElement))
+	{
+		unsortedArr[unsortedArr.size() - 1] = firstElement;
+		unsortedArr[0] = lastElement;
+	}
+	//If the middle element if pivot, move it to end
+	else if ((middleElement > firstElement && middleElement < lastElement) || (middleElement < firstElement && middleElement > lastElement))
+	{
+		unsortedArr[unsortedArr.size() - 1] = middleElement;
+		unsortedArr[unsortedArr.size() / 2] = lastElement;
+	}
+	return unsortedArr[unsortedArr.size() - 1];
 }
 
 
 int main()
 {
-	std::vector<int> unsortedVec = { 2, 6, 5, 3, 8, 7, 1, 0 };
+	//std::vector<int> unsortedVec = { 2, 6, 5, 3, 8, 7, 1, 0 };
+	std::vector<int> unsortedVec = { 3, 5, 2, 1 };
 	//std::vector<int> unsortedVec;
 	//for (int i = 0; i < 100000; ++i)
 	//	unsortedVec.emplace_back(rand());
 
 
 	std::vector<int> sortedVec = QuickSort(unsortedVec);
+
+	//std::vector<int> vec = { 1, 2, 3, 5 };
+	//std::cout << GetPivot(vec);
 
 	std::cout << sortedVec << '\n';
 
